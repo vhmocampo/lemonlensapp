69c87ab3c29d3abbf6c7c29ab4b9dfdf05ae4958
Victor M <vmocampo357@gmail.com>
2025-05-18 00:18:58 -0500

    small update

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
-use Illuminate\Support\Facades\Request;
+use Illuminate\Http\Request;
 
 Route::post('/reports', [ReportController::class, 'store']);            // create report
 
