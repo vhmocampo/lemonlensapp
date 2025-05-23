106d3235a12fd4658b995e479dfd822cb8a0ad0c
Victor M <vmocampo357@gmail.com>
2025-05-22 22:02:52 -0500

    continue optional middleware

---
+++ app/Middleware/OptionalSanctumAuth.php
@@ -0,0 +1,37 @@
+<?php
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Laravel\Sanctum\Sanctum;
+
+class OptionalSanctumAuth
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        // Attempt to authenticate the user, but don't require it
+        if ($request->bearerToken()) {
+            $model = Sanctum::$personalAccessTokenModel;
+            $accessToken = $model::findToken($request->bearerToken());
+            
+            if ($accessToken && !$accessToken->expired()) {
+                $user = $accessToken->tokenable;
+                if ($user) {
+                    auth()->setUser($user);
+                }
+            }
+        }
+        
+        // Continue with the request, even if authentication failed
+        return $next($request);
+    }
+}
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias([
+            'sanctum.optional' => OptionalSanctumAuth::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
